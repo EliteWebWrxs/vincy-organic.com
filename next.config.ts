9cdb55c56064fe763dc2ce9e144bc7068d2b0136
Kevin Halstead <elitewebwrxs@gmail.com>
2025-06-16 14:01:03 -0400

    update

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,19 @@ const nextConfig: NextConfig = {
     // Matches the behavior of `sanity dev` which sets styled-components to use the fastest way of inserting CSS rules in both dev and production. It's default behavior is to disable it in dev mode.
     SC_DISABLE_SPEEDY: "false",
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["placeholder.svg"],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
